ac32a0efbd4d9276e9f8f2552f783261b2aa1608
zhangyunwu <64191548+zhangyunwu@users.noreply.github.com>
2020-06-18 09:35:13 +0800

    Add files via upload
    添加多线程操作

--- Spider_m131.py
+++ Spider_m131.py
@@ -1,6 +1,10 @@
 import os
 import requests
+import threading
+import time
+from concurrent.futures import ThreadPoolExecutor, as_completed
 from lxml import etree
+
 
 class Spider_mm(object):
     """爬取妹子图"""
@@ -87,51 +91,83 @@ class Spider_mm(object):
                 f.write(img_data.content)
                 f.close
 
+            time.sleep(0.2)
+    
+
+
+def delete_empty_dir(path):
+    """删除传入路径下所有空文件夹"""
+
+    # 获取path文件夹下所有子文件夹
+    dirs = next(os.walk(path))[1]
+    for item in dirs:
+        dir = os.path.join(path, item)
+        try:
+            # 删除指定文件夹（当该文件夹为空文件夹时）
+            os.rmdir(dir)
+        except:
+            pass
+
+
+def download_thread(spider, pic_set_title, pic_set_url):
+    set_pic_path = PICTURE_PATH + pic_set_title
+    try:
+        os.mkdir(set_pic_path)
+    except:
+        print(pic_set_title, '---文件夹已存在\n')
+        return
+
+    pic_urls = spider.get_pic_urls(pic_set_url)
+    print(pic_set_title, '---开始下载>>>\n')
+    spider.download_pic(pic_urls, set_pic_path)
+    print(pic_set_title, '---下载完成\n')
+
 
 
 def main():
-    print('开始啦')
+    print('开始啦，开始时间：')
+    print(time.ctime())
+    t_start = time.time()
 
-    PICTURE_PATH = os.path.join(os.getcwd(), 'pictures/')
     try:
         os.mkdir(PICTURE_PATH)
     except:
         pass
+
+    # 在这里增加一个删除空文件夹的操作
+    delete_empty_dir(PICTURE_PATH)
 
     # 爬取页数
     page_num = 50
 
     spider = Spider_mm(page_num)
 
-
     page_urls = spider.get_page_urls()
     for page_url in page_urls:
 
         # 获取套图标题及地址
         [pic_set_titiles, pic_set_urls] = spider.get_pic_set_urls(page_url)
+        all_task = []
+        with ThreadPoolExecutor(8) as executor:
+            for n, pic_set_url in enumerate(pic_set_urls):
+                pic_set_title = pic_set_titiles[n]
+                task = executor.submit(download_thread, spider, pic_set_title, pic_set_url)
+                all_task.append(task)
+                time.sleep(1)
+        for _ in as_completed(all_task):
+            pass
 
-        for n, pic_set_url in enumerate(pic_set_urls):
+    print('完成啦，完成时间：')
+    print(time.ctime())
+    t_end = time.time()
+    print('总耗时：', t_end - t_start)
 
-            # 套图存储路径
-            set_pic_path = PICTURE_PATH + pic_set_titiles[n]
-            try:
-                os.mkdir(set_pic_path)
-            except:
-                print('文件夹已存在')
-                continue
 
-            pic_urls =spider.get_pic_urls(pic_set_url)
-            spider.download_pic(pic_urls, set_pic_path)
-
-    print('结束啦')
 
 if __name__ == '__main__':
+    global PICTURE_PATH
+
+    PICTURE_PATH = os.path.join(os.getcwd(), 'pictures/')
+
     main()
-
-
-
-    
-
-
-
 
